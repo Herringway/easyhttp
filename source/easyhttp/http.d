e6c864504a5ed61477c5da179103f09bbb7101ce
Cameron Ross <elpenguino@gmail.com>
2022-03-10 13:48:39 -0400

    give cookie a nice toString

--- source/easyhttp/http.d
+++ source/easyhttp/http.d
@@ -144,6 +144,10 @@ struct Cookie {
 	string path;
 	string key;
 	string value;
+	void toString(T)(T sink) const if (isOutputRange!(T, char[])) {
+		import std.format : formattedWrite;
+		sink.formattedWrite!"%s=%s"(key, value);
+	}
 }
 
 struct HTTPHeader {
